78f0d2169d05bc4eafa64dfca4560dbe817a5f18
Neeraj Kumar <neerajkumar.nitt@gmail.com>
2021-03-11 22:00:00 +0530

    uninstall.sh fixed

--- uninstall.sh
+++ uninstall.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 files_dir=$(find /usr/local/ -name "sysmontask*")
 for file in $files_dir
 do
 	sudo rm -rf $file
 done
-sudo rm -rf /usr/applicatons/SysMonTask.desktop
+sudo rm -rf /usr/share/applications/SysMonTask.desktop
 sudo rm -rf /usr/share/sysmontask
 sudo rm -rf /usr/share/doc/sysmontask
 echo "Done"
