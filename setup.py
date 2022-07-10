e3e63038fc9ffc94af4f6dbc2574a4b622e292f5
IrenvanderBeekPXL <Iren.vanderbeek@student.pxl.be>
2022-07-10 19:56:49 +0200

    Fixed version and gitignore for Jetbrains IDE

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_data_files():
 
 setup(
     name='sysmontask',
-    version='1.x.x',
+    version='2.0.0',
     description='System Monitor With UI Like Windows',
     url='https://github.com/KrispyCamel4u/SysMonTask',
     author='Neeraj Kumar',
